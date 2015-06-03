e0d1c759e283672c196189b2d6a8844dc3fd12b4
sunnyshrestha <dev.suncha@gmail.com>
2015-06-03 17:35:06 +0545

    onSaveInstance coded Persistent alertdialogs remaining
    create dialogs using dialogfragment
    developer.android.com/guide/topics/ui/dialogs.html

--- app/src/main/java/dev/suncha/myleads/saveDialogFragment.java
+++ app/src/main/java/dev/suncha/myleads/saveDialogFragment.java
@@ -3,6 +3,8 @@ package dev.suncha.myleads;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
 
 /**
  * Created by Sunny on 5/28/2015.
@@ -11,23 +13,21 @@ public class saveDialogFragment extends DialogFragment {
 
     @Override
     public android.app.Dialog onCreateDialog(Bundle savedInstanceState) {
-        return new android.support.v7.app.AlertDialog.Builder(getActivity())
-                .setIcon(android.R.drawable.ic_menu_save)
-                .setTitle(R.string.discard)
-                .setMessage(R.string.discard_message)
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater layoutInflater = getActivity().getLayoutInflater();
+        builder.setView(layoutInflater.inflate(R.layout.layout_discard_draft, null))
                 .setPositiveButton("DISCARD", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         getActivity().finish();
                     }
                 })
-                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                .setNegativeButton("CONTINUE EDITING", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
 
                     }
-                }).create();
+                });
+        return builder.create();
     }
-
-
 }
