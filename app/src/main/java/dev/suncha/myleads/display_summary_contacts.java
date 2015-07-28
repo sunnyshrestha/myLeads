d769d0dbbd1a0f96cbdc49f61e4660fb3b7ddbc9
sunnyshrestha <dev.suncha@gmail.com>
2015-07-28 09:03:07 +0545

    Fragments are killed when touched outside - fix it //FIXED
    make size of switch equal to that of surrounding buttons
    
    if bundled name and company name are empty, handle that
    
    check that the date is valid. it is just checking the format now.//FIXED
    
    edit activity should allow editing the event.
    
    trying to use green dao

--- app/src/main/java/dev/suncha/myleads/display_summary_contacts.java
+++ app/src/main/java/dev/suncha/myleads/display_summary_contacts.java
@@ -1,8 +1,12 @@
 package dev.suncha.myleads;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,6 +17,9 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.amulyakhare.textdrawable.TextDrawable;
+import com.amulyakhare.textdrawable.util.ColorGenerator;
+
 import java.util.ArrayList;
 
 public class display_summary_contacts extends AppCompatActivity {
@@ -21,29 +28,58 @@ public class display_summary_contacts extends AppCompatActivity {
     ArrayList<Lead> leadArrayList = new ArrayList<Lead>();
     DatabaseHelper db;
     newAdapter leadAdapter;
+    FloatingActionButton fab;
+    private ColorGenerator mColorGenerator = ColorGenerator.MATERIAL;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_previous_listview);
+        try {
+            summary = (ListView) findViewById(R.id.list);
+            summary.setItemsCanFocus(false);
+            fab = (FloatingActionButton) findViewById(R.id.buttonFloat);
+            setupToolbar();
+        } catch (Exception e) {
+            Log.e("Exception occured ", "" + e);
+        }
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent addLead = new Intent(getApplicationContext(), AddLeadDetail.class);
+                startActivity(addLead);
+            }
+        });
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(0xFFFFFFFF);
+        setSupportActionBar(toolbar);
+        //Show menu icon
+        //final ActionBar actionBar=getSupportActionBar();
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        refreshData();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+
         getMenuInflater().inflate(R.menu.menu_display_summary_contacts, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
@@ -75,10 +111,7 @@ public class display_summary_contacts extends AppCompatActivity {
         leadAdapter = new newAdapter(display_summary_contacts.this, R.layout.display_summary_listview, leadArrayList);
         summary.setAdapter(leadAdapter);
         leadAdapter.notifyDataSetChanged();
-
-
     }
-
 
     public class newAdapter extends ArrayAdapter<Lead> {
         Activity activity;
@@ -104,13 +137,32 @@ public class display_summary_contacts extends AppCompatActivity {
                 LayoutInflater inflater = LayoutInflater.from(activity);
                 row = inflater.inflate(layoutResourceId, parent, false);
                 holder = new LeadHolder();
-                holder.
+                holder.companyname = (TextView) row.findViewById(R.id.companyname);
+                holder.personname = (TextView) row.findViewById(R.id.personname);
+                holder.personphone = (TextView) row.findViewById(R.id.personphone);
+                holder.personemail = (TextView) row.findViewById(R.id.personemail);
+                holder.imageView = (ImageView) row.findViewById(R.id.imageView);
+                holder.checkedIcon = (ImageView) row.findViewById(R.id.checkIcon);
+                row.setTag(holder);
             } else {
                 holder = (LeadHolder) row.getTag();
             }
-            mlead = lead.get(position);
-            holder.
 
+            TextDrawable drawable = TextDrawable.builder()
+                    .buildRect(String.valueOf(mlead.getCompany_name().charAt(0)), mColorGenerator.getRandomColor());
+
+            TextDrawable color = TextDrawable.builder()
+                    .buildRect(" ", mColorGenerator.getColor("Selected"));
+
+            mlead = lead.get(position);
+            holder.imageView.setImageDrawable(drawable);
+            holder.checkedIcon.setImageDrawable(color);
+            holder.companyname.setText(mlead.getCompany_name());
+            holder.personname.setText(mlead.getPerson_name());
+            holder.personphone.setText(mlead.getPerson_mobile());
+            holder.personemail.setText(mlead.getPerson_email());
+
+            return row;
         }
 
         class LeadHolder {
